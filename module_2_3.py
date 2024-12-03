8e5817dce8afcd7500f9dc1a9556d683ec119014
GlebPython <gleb51339@gmail.com>
2024-12-03 10:01:27 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 
-while index <= len(my_list):
+while index < len(my_list):
     elem = my_list[index]
     index += 1  # придаёт индексу шаг (сокращение от index = index + 1)
     if elem == 0:
